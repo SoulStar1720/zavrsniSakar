2b197f84a6fb4457055ea5756206a3d75cbcc02e
Lena Sakar <sakarlena@gmail.com>
2026-05-16 16:09:04 +0200

    izbrisano nešt

--- includes/auth.php
+++ includes/auth.php
@@ -1,20 +1,3 @@
-<!--/*
-// Sigurnosne postavke za sesije
-//Ovo pa sve nadalje su neeke sigurnosti ko za pravu stranicu 
-ini_set('session.cookie_httponly', 1); //Onemogućava JavaScriptu pristup session cookie-u (npr. document.cookie neće vidjeti cookie).
-ini_set('session.cookie_secure', 1); // Koristi samo ako je HTTPS //Šalje cookie samo preko HTTPS veze (ignorira ga na HTTP).
-ini_set('session.use_strict_mode', 1); //Prihvaća samo unaprijed inicirane ID-eve sesija.
-
-// Zaštita od session fixation
-if (!isset($_SESSION['initiated'])) {  //Generira novi ID sesije pri svakoj prijavi i briše stari.
-    session_regenerate_id(true);
-    $_SESSION['initiated'] = true;
-}
-
-// Zaštita od XSS (Cross-Site Scripting) u ispisu podataka iz sesije
-function escapeSessionData($data) {                             //Sprječava XSS napade ako bi napadač uspio ubaciti štetni kod u podatke sesije.
-    return htmlspecialchars($data, ENT_QUOTES, 'UTF-8');
-}*/-->
 <?php
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
