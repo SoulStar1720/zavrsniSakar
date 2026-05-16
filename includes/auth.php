40520050a030c8af99063c8f47edf9661704b05c
Lena Sakar <sakarlena@gmail.com>
2026-05-16 16:09:48 +0200

    izbrisano nešt

--- includes/auth.php
+++ includes/auth.php
@@ -35,14 +35,3 @@ function redirectBasedOnRole() {
     exit();
 }
 ?>
-<!--/*služi za zaštitu od CSRF napada (Cross-Site Request Forgery).
-function generateCsrfToken() {
-    if (empty($_SESSION['csrf_token'])) {
-        $_SESSION['csrf_token'] = bin2hex(random_bytes(32));
-    }
-    return $_SESSION['csrf_token'];
-}
-
-function validateCsrfToken($token) {
-    return hash_equals($_SESSION['csrf_token'] ?? '', $token);
-}*/-->
