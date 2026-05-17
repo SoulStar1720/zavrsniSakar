84ac2f6e2063875a263181750f5ece32775c3ffb
Lena Sakar <sakarlena@gmail.com>
2026-05-17 10:40:07 +0200

    izbrisano nešt

--- detalji_knjige.php
+++ detalji_knjige.php
@@ -23,7 +23,6 @@ $putanjaSlike = "naslovnice/" . $imeSlike;
 <body>
     <div class="container py-5">
         <a href="index.php" class="btn btn-link mb-4 p-0 text-decoration-none">← Povratak na popis</a>
-        
         <div class="row">
             <div class="col-md-4 mb-4">
                 <img src="<?php echo $putanjaSlike; ?>" class="img-fluid rounded shadow" alt="<?php echo $knjiga['naslov']; ?>">
@@ -32,7 +31,6 @@ $putanjaSlike = "naslovnice/" . $imeSlike;
                 <h1 class="display-5 fw-bold"><?php echo $knjiga['naslov']; ?></h1>
                 <p class="fs-4 text-muted"><?php echo $knjiga['autor']; ?></p>
                 <hr>
-                
                 <table class="table table-borderless">
                     <tr>
                         <th width="150">Izdavač:</th>
@@ -51,12 +49,10 @@ $putanjaSlike = "naslovnice/" . $imeSlike;
                         <td><?php echo $knjiga['broj_primjeraka']; ?> kom</td>
                     </tr>
                 </table>
-
                 <div class="mt-4 p-4 bg-light rounded">
                     <h5>Opis knjige</h5>
                     <p><?php echo nl2br($knjiga['opis']); ?></p>
                 </div>
-
                 <div class="mt-4 gap-2 d-flex">
                     <a href="views/posudbe/posudi.php?knjiga_id=<?php echo $id; ?>" class="btn btn-primary btn-lg px-5">Posudi knjigu</a>
                     <a href="rezerviraj.php?id=<?php echo $id; ?>" class="btn btn-outline-info btn-lg px-5">Rezerviraj</a>
